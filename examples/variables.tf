2396a36ab2a1d4d05c5d2af800d55266ce9e37c7
Théo Bori <theo1.bori@epitech.eu>
2024-06-05 03:34:01 +0200

    🎨 (example): More flexibility with the Terraform project

--- examples/variables.tf
+++ examples/variables.tf
@@ -1,5 +1,11 @@
 variable "base_path" {
   description = "Base path for the local files"
   type        = string
-  default     = "/tmp/tests"
+  default     = "/tmp"
+}
+
+variable "files_amount" {
+  description = "Amount of created local files"
+  type        = number
+  default     = 5
 }
